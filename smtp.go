2babca8246d1746529dcc6b318660259b22be8ac
Daniel Margolis <dan@af0.net>
2017-02-05 22:45:28 +0000

    Specify client hostname in EHLO.

--- smtp.go
+++ smtp.go
@@ -5,13 +5,32 @@ import (
 	"fmt"
 	"net"
 	"net/smtp"
+	"os"
 	"strings"
 )
+
+func helloHostName() (string, error) {
+	// Get the hostname for HELO/EHLO.
+	h, err := os.Hostname()
+	if err != nil {
+		return h, err
+	}
+	// TODO: Do something more sophisticated here to try to guess the FQDN, I guess.
+	return h, err
+}
 
 // CheckMXViaSMTP returns an error if a secure connection to the MX cannot be established.
 func CheckMXViaSMTP(mx *net.MX) error {
 	c, err := smtp.Dial(strings.TrimSuffix(mx.Host, ".") + ":25")
 	if err != nil {
+		return err
+	}
+	// Set hostname manually, because some hosts reject 'localhost', which is the default.
+	h, err := helloHostName()
+	if err != nil {
+		return err
+	}
+	if err := c.Hello(h); err != nil {
 		return err
 	}
 	if err = c.StartTLS(&tls.Config{
@@ -23,5 +42,6 @@ func CheckMXViaSMTP(mx *net.MX) error {
 	if _, ok := c.TLSConnectionState(); !ok {
 		return fmt.Errorf("Could not negotiate TLS with %v\n", mx.Host)
 	}
+	return c.Quit()
 	return nil
 }
